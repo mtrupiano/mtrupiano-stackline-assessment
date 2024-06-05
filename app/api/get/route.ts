6ea07e8bfa58a2fa306d8cc96949b24d1cf964c3
mtrupiano <mtru.dev@gmail.com>
2024-06-05 14:44:07 -0700

    change api route to return single product object instead of single-entry array

--- app/api/get/route.ts
+++ app/api/get/route.ts
@@ -2,5 +2,6 @@ import { NextApiRequest } from "next";
 
 import data from "./stackline_frontend_assessment_data_2021.json"
 export async function GET(req: NextApiRequest) {
-  return Response.json(data);
+  return Response.json(data[0]);
+  // Not sure why the json file returns a single-entry array instead of just the object...
 }
