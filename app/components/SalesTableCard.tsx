68c7f15829070b12645e87e91789ffbd20e69475
mtrupiano <mtru.dev@gmail.com>
2024-06-05 16:32:03 -0700

    tweak table styling

--- app/components/SalesTableCard.tsx
+++ app/components/SalesTableCard.tsx
@@ -3,6 +3,10 @@ import { Card } from "@mui/material";
 import { DataGrid, type GridColDef } from "@mui/x-data-grid";
 import { useAppSelector } from "@/lib/hooks";
 
+function formatPrice(value: number): string {
+  return '$' + new Intl.NumberFormat().format(value);
+}
+
 export default function SalesTableCard() {
   const data = useAppSelector(state => state.product.sales);
 
@@ -12,39 +16,53 @@ export default function SalesTableCard() {
       headerName: "Week Ending",
       resizable: false,
       flex: 1,
+      align: "center",
+      headerAlign: "center",
     },
     {
       field: "retailSales",
       headerName: "Retail Sales",
       resizable: false,
       flex: 1,
+      valueFormatter: formatPrice,
+      align: "center",
+      headerAlign: "center",
     },
     {
       field: "wholesaleSales",
       headerName: "Wholesale Sales",
       resizable: false,
       flex: 1,
+      valueFormatter: formatPrice,
+      align: "center",
+      headerAlign: "center",
     },
     {
       field: "unitsSold",
       headerName: "Units Sold",
       resizable: false,
       flex: 1,
+      align: "center",
+      headerAlign: "center",
     },
     {
       field: "retailerMargin",
       headerName: "Retailer Margin",
       resizable: false,
       flex: 1,
+      valueFormatter: formatPrice,
+      align: "center",
+      headerAlign: "center",
     },
   ];
 
   return (
-    <Card>
+    <Card style={{ height: "1000px" }}>
       <DataGrid 
         columns={columns}
         rows={data}
         getRowId={row => row.weekEnding} // assuming only one entry per week for unique row ID
+        // autoHeight={false}
       />
     </Card>
   );
